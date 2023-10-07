857909f3007c4da199e2a825e91ff4a66caa4acd
quantized-cube <yukimath@gmail.com>
2023-10-07 21:17:56 +0900

    トップページを別に作って、整理

--- app/page.tsx
+++ app/page.tsx
@@ -1,77 +1,21 @@
 'use client'
 
-import { useState } from 'react';
 import Head from 'next/head';
-
-const questions = [
-  '質問1: このテストは役に立つと思いますか？',
-  '質問2: このテストは難しいと思いますか？',
-  '質問3: このテストは楽しいと思いますか？',
-  '質問4: このテストは悲しいと思いますか？',
-];
+import Link from 'next/link'
 
 export default function Home() {
-  const [scores, setScores] = useState<number[]>(Array(questions.length).fill(0));
-  const [showResults, setShowResults] = useState(false); // 結果を表示するための状態
-  const shouldShowResultsButton = scores.every((score) => score !== 0); // scoresに0が含まれていないかチェック
-
-  const handleAnswer = (index: number, score: number) => {
-    const newScores = [...scores];
-    newScores[index] = score;
-    setScores(newScores);
-  };
-
-  const totalScore = scores.reduce((acc, score) => acc + score, 0);
-  const totalScore_1 = scores.slice(0, 2).reduce((acc, score) => acc + score, 0);
-  const totalScore_2 = scores.slice(2, 4).reduce((acc, score) => acc + score, 0);
-  const resultMessage = `合計スコア ${totalScore}`;
-  const resultMessage1 = `1と2の合計スコア ${totalScore_1}`;
-  const resultMessage2 = `3と4の合計スコア ${totalScore_2}`;
-
-  const handleShowResults = () => {
-    // 結果を表示するボタンをクリックしたら結果を表示
-    setShowResults(true);
-  };
-
   return (
     <div>
       <Head>
-        <title>心理テスト</title>
+        <title>心理テストまとめ</title>
         <meta name="description" content="心理テストのサンプル" />
       </Head>
 
       <main>
-        <h1>心理テスト</h1>
-        {questions.map((question, index) => (
-          <div key={index}>
-            <h2>{question}</h2>
-            {[1, 2, 3, 4, 5, 6].map((score) => (
-              <button
-                key={score}
-                onClick={() => handleAnswer(index, score)}
-                className={scores[index] === score ? 'selected' : ''}
-              // disabled={showResults} // 結果表示中はボタンを無効化
-              >
-                {score}
-              </button>
-            ))}
-          </div>
-        ))}
-        {shouldShowResultsButton && (
-          <div>
-            <button onClick={handleShowResults}>結果を表示</button>
-          </div>
-        )}
-        {showResults && ( // 結果を表示する場合に表示
-          <div>
-            <h2>結果</h2>
-            <p>テスト結果：{resultMessage}</p>
-            <h3>結果1</h3>
-            <p>{resultMessage1}</p>
-            <h3>結果2</h3>
-            <p>{resultMessage2}</p>
-          </div>
-        )}
+        <h1>心理テストまとめ</h1>
+        <Link href="/test01">
+          心理テスト01
+        </Link>
       </main>
     </div>
   );
